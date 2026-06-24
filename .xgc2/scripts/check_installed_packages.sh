e792a8cda9d4556b309592a680a26c96f50f1dae
Lxk_buaa <867768510@qq.com>
2026-06-25 02:07:11 +0800

    Migrate ROS1 utils to xgc2 math

--- .xgc2/scripts/check_installed_packages.sh
+++ .xgc2/scripts/check_installed_packages.sh
@@ -5,7 +5,7 @@ ROS_DISTRO="${ROS_DISTRO:-noetic}"
 PREFIX="/opt/ros/${ROS_DISTRO}"
 
 dpkg -s ros-noetic-xgc2-ros1-utils >/dev/null
-dpkg -s libxgc2-observer-dev >/dev/null
+dpkg -s libxgc2-math-dev >/dev/null
 rospack find ros1_utils >/dev/null
 test -f "${PREFIX}/include/ros1_utils/loop_controller.h"
 test -f "${PREFIX}/include/ros1_utils/namespace_utils.h"
@@ -22,7 +22,7 @@ test -f "${PREFIX}/include/controller_runtime/time/loop_controller.h"
 test -f "${PREFIX}/include/controller_runtime/time/tick_context.h"
 test -f "${PREFIX}/include/control_utils/butterworth_filter.h"
 test -f "${PREFIX}/include/control_utils/ugv_identification.h"
-test -f /usr/include/xgc2_observer/butterworth_filter.hpp
+test -f /usr/include/xgc2_math/filter/butterworth_filter.hpp
 test -f "${PREFIX}/lib/libros1_utils_ugv_identification.so"
 
 while IFS= read -r file; do
--- include/control_utils/butterworth_filter.h
+++ include/control_utils/butterworth_filter.h
@@ -1,11 +1,11 @@
 #ifndef CONTROL_UTILS_BUTTERWORTH_FILTER_H
 #define CONTROL_UTILS_BUTTERWORTH_FILTER_H
 
-#include <xgc2_observer/butterworth_filter.hpp>
+#include <filter/butterworth_filter.hpp>
 
 namespace control_utils {
 
-using SecondOrderButterworthLowPass = xgc2_observer::SecondOrderButterworthLowPass;
+using SecondOrderButterworthLowPass = xgc2_math::SecondOrderButterworthLowPass;
 
 }  // namespace control_utils
 
